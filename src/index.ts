d91b989c48aacec243e4fcd7ae29feb9f85fe32c
Karol <qxtno@outlook.com>
2020-06-04 19:36:29 +0200

    Added button and input to read data

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
-console.log("hello world!");
+const button = document.querySelector('#butonek');
+const input: HTMLInputElement = document.querySelector(
+  '#location'
+) as HTMLInputElement;
 
-console.log("tu byłem");
+const baseUrl = 'https://api.openweathermap.org/data/2.5';
+const appId = '&appid=81e564bdda8adddbc2d805694d19cdac';
+const unitsType = '&units=metric';
+input.value = 'lublin';
+
+button?.addEventListener('click', async () => {
+  const location = input.value;
+  if (location == null || location == '') {
+    alert('Pole nie może być puste');
+  } else {
+    const weatherData = await getWeatherData(location);
+    if (weatherData.cod == '404') {
+      alert(`Nie znaleziono prognozy dla ${location}`);
+    }
+    console.log('data', weatherData);
+  }
+});
+
+async function getWeatherData(location: string) {
+  const response = await fetch(
+    `${baseUrl}/weather?q=${location}${unitsType}${appId}`
+  );
+  return response.json();
+}
